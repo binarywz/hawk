f6a9c574e546bb3263d394b620ce89216f165ac7
cuiwz <cuiwzupc@163.com>
2021-08-21 09:53:08 +0800

    构建网关

---
+++ hawk-gateway/src/main/java/binary/wz/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package binary.wz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author cuiwz
+ * @Date 2021/8/21 9:46
+ * @Description:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
